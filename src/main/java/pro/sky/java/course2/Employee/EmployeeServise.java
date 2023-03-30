12955431769516cec7175191100238350a30bbdc
AlexanderElkin1969 <eaa@mfcmmr.ru>
2023-03-31 01:24:19 +0300

    with ArrayList

--- src/main/java/pro/sky/java/course2/Employee/EmployeeServise.java
+++ src/main/java/pro/sky/java/course2/Employee/EmployeeServise.java
@@ -2,75 +2,57 @@ package pro.sky.java.course2.Employee;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Service
 public class EmployeeServise {
- //  static Employee[] employees = new Employee[10];
-    public static Employee[] employees = {
+    public static List<Employee> employees = new ArrayList<>(Arrays.asList(
             new Employee("Александр", "Александров"),
             new Employee("Александр", "Борисов"),
             new Employee("Владимир", "Борисов"),
             new Employee("Александр", "Иванов"),
             new Employee("Иван", "Иванов"),
             new Employee("Александр", "Егоров"),
-            new Employee("Александр", "Петров"),
-            null,
-            null,
-            null,
-    };
+            new Employee("Александр", "Петров")
+            ));
 
     public Employee findService(String firstName, String lastName) throws EmployeeNotFoundException{
         Employee buffer = new Employee(firstName, lastName);
-        if (employees.length == 0){
+        if (employees.size() == 0){
             throw  new EmployeeNotFoundException("Список сотрудников пуст.");
         }
-        for (int i = 0; i < employees.length; i++) {
-            if (employees[i] == null){
-                continue;
-            }
-            if (employees[i].equals(buffer)){
-                return employees[i];
+        for (Employee employee : employees) {
+            if (employee.equals(buffer)){
+                return employee;
             }
         }
         throw new EmployeeNotFoundException(buffer + " в списке сотрудников отсутствует.");
     }
 
     public Employee addService(String firstName, String lastName) throws EmployeeAlreadyAdded, EmployeeStorageIsFullException {
-        int freeElement = -1;
-        try {
-            for (int i = 0; i < employees.length; i++) {
-                if (employees[i] == null){
-                    freeElement = i;
-                    break;
-                }
+        Employee buffer = new Employee(firstName, lastName);
+        if (employees.size() == 10){
+                throw  new EmployeeStorageIsFullException("Список сотрудников переполнен. Число сотрудников по штату не может превышать 10 человек.");
             }
-            if (freeElement == -1){
-                throw  new EmployeeStorageIsFullException("Список сотрудников переполнен. Невозможно добавить нового сотрудника.");
-            }
-            if (findService(firstName, lastName) != null) {
+        if (employees.contains(buffer)) {
                 throw  new EmployeeAlreadyAdded(firstName + " " + lastName + " уже есть в списке сотрудников.");
             }
-        }catch (EmployeeNotFoundException e){
-            employees[freeElement] = new Employee(firstName, lastName);
-                return employees[freeElement];
-        }
-        return employees[0];
+            employees.add(buffer);
+        return buffer;
     }
 
     public Employee removeService(String firstName, String lastName) throws EmployeeNotFoundException{
         Employee buffer = new Employee(firstName, lastName);
-        if (employees.length == 0){
+        if (employees.size() == 0){
             throw  new EmployeeNotFoundException("Список сотрудников пуст.");
         }
-        for (int i = 0; i < employees.length; i++) {
-            if (employees[i] == null){
-                continue;
-            }
-            if (buffer.equals(employees[i])){
-                employees[i] = null;
-                return buffer;
-            }
+        if (!employees.contains(buffer)) {
+            throw  new EmployeeNotFoundException(buffer + " в списке сотрудников отсутствует.");
         }
-        throw new EmployeeNotFoundException(firstName + " " + lastName + " в списке сотрудников отсутствует.");
+        employees.remove(buffer);
+        return buffer;
     }
 
 }
